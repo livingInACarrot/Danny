a621245d474e7541b11301aed3db5091a203ed57
Tatyana Guzovskaya <guzovskaya.tanya.gogle@gmail.com>
2025-10-26 14:55:26 +0300

    fix: cards behaviour

--- Danny/Assets/Scripts/Card.cs
+++ Danny/Assets/Scripts/Card.cs
@@ -1,170 +1,108 @@
+using System.ComponentModel;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.InputSystem;
+using UnityEngine.UI;
 
 public class Card : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler, IPointerDownHandler, IPointerUpHandler, IDragHandler, IScrollHandler
 {
     [Header("Visual Settings")]
-    [SerializeField] private SpriteRenderer cardSprite;
-    [SerializeField] private SpriteRenderer highlightBorder;
     [SerializeField] private int normalSortingOrder = 0;
 
     [Header("Rotation Settings")]
     [SerializeField] private float rotationSpeed = 5f;
     [SerializeField] private float maxRotationAngle = 45f;
 
-    [Header("Movement Settings")]
-    [SerializeField] private float dragSpeed = 1f;
-    [SerializeField] private bool snapToGrid = false;
-    [SerializeField] private float gridSize = 0.1f;
-
+    private Canvas canvas;
     private bool isDragging = false;
     private bool isHighlighted = false;
-    private Vector3 offset;
-    private int originalSortingOrder;
-    private float originalZRotation;
+    private Vector2 offset;
 
     private void Start()
     {
-        // Инициализация компонентов
-        if (highlightBorder != null)
-        {
-            highlightBorder.gameObject.SetActive(false);
-        }
-        cardSprite = GetComponent<SpriteRenderer>();
-        originalSortingOrder = cardSprite.sortingOrder;
-        originalZRotation = transform.rotation.eulerAngles.z;
+        canvas = GetComponentInParent<Canvas>();
     }
 
-    // Обработка наведения курсора
     public void OnPointerEnter(PointerEventData eventData)
     {
         Debug.Log("Pointer Enter");
         isHighlighted = true;
-        if (highlightBorder != null)
-        {
-            highlightBorder.gameObject.SetActive(true);
-        }
     }
 
-    // Обработка ухода курсора
     public void OnPointerExit(PointerEventData eventData)
     {
         isHighlighted = false;
-        if (highlightBorder != null && !isDragging)
-        {
-            highlightBorder.gameObject.SetActive(false);
-        }
     }
-
-    // Обработка нажатия на карту
+      
     public void OnPointerDown(PointerEventData eventData)
     {
         if (eventData.button == PointerEventData.InputButton.Left)
         {
             isDragging = true;
-
-            // Вычисляем смещение для плавного перемещения
-            Vector3 mouseWorldPos = GetMouseWorldPosition();
-            offset = transform.position - mouseWorldPos;
+            if (RectTransformUtility.ScreenPointToLocalPointInRectangle(
+                canvas.transform as RectTransform,
+                eventData.position,
+                canvas.worldCamera,
+                out Vector2 localPointerPosition))
+            {
+                offset = GetComponent<RectTransform>().anchoredPosition - localPointerPosition;
+            }
         }
     }
 
-    // Обработка отпускания кнопки мыши
     public void OnPointerUp(PointerEventData eventData)
     {
         if (eventData.button == PointerEventData.InputButton.Left)
         {
             isDragging = false;
-            cardSprite.sortingOrder = originalSortingOrder;
-
-            if (!isHighlighted && highlightBorder != null)
-            {
-                highlightBorder.gameObject.SetActive(false);
-            }
         }
     }
 
-    // Обработка перетаскивания
     public void OnDrag(PointerEventData eventData)
     {
         if (isDragging && eventData.button == PointerEventData.InputButton.Left)
         {
-            Vector3 newPosition = GetMouseWorldPosition() + offset;
-
-            if (snapToGrid)
+            if (RectTransformUtility.ScreenPointToLocalPointInRectangle(
+                canvas.transform as RectTransform,
+                eventData.position,
+                canvas.worldCamera,
+                out Vector2 localPointerPosition))
             {
-                newPosition.x = Mathf.Round(newPosition.x / gridSize) * gridSize;
-                newPosition.y = Mathf.Round(newPosition.y / gridSize) * gridSize;
+                Vector2 newPosition = localPointerPosition + offset;
+                GetComponent<RectTransform>().anchoredPosition = newPosition;
             }
-
-            transform.position = Vector3.Lerp(transform.position, newPosition, dragSpeed * Time.deltaTime);
         }
     }
 
-    // Обработка прокрутки колесика мыши
     public void OnScroll(PointerEventData eventData)
     {
         if (!isHighlighted) return;
 
         float scrollDelta = eventData.scrollDelta.y;
 
-        if (Input.GetKey(KeyCode.LeftControl) || Input.GetKey(KeyCode.RightControl))
+        if (IsCtrlPressed())
         {
-            // Изменение порядка в слое при удержании Ctrl
-            cardSprite.sortingOrder += (int)Mathf.Sign(scrollDelta);
-            originalSortingOrder = cardSprite.sortingOrder;
+            canvas.sortingOrder -= (int)Mathf.Sign(scrollDelta);
+            Debug.Log($"Новый порядок слоя: {canvas.sortingOrder}");
         }
         else
         {
-            // Изменение вращения по Z
             float currentRotation = transform.rotation.eulerAngles.z;
-            float newRotation = currentRotation + scrollDelta * rotationSpeed;
+            if (currentRotation > 180f) currentRotation -= 360f;
 
-            // Ограничиваем угол вращения
-            if (newRotation > 180f) newRotation -= 360f;
-            newRotation = Mathf.Clamp(newRotation, -maxRotationAngle, maxRotationAngle);
-
-            transform.rotation = Quaternion.Euler(0f, 0f, newRotation);
+            transform.rotation = Quaternion.Euler(0f, 0f, currentRotation + scrollDelta * rotationSpeed);
         }
     }
 
-    // Вспомогательный метод для получения позиции мыши в мировых координатах
-    private Vector3 GetMouseWorldPosition()
+    private bool IsCtrlPressed()
     {
-        Vector3 mousePos = Input.mousePosition;
-        mousePos.z = -Camera.main.transform.position.z;
-        return Camera.main.ScreenToWorldPoint(mousePos);
-    }
+        if (Mouse.current == null) return false;
 
-    // Публичные методы для управления подсветкой
-    public void EnableHighlight()
-    {
-        if (highlightBorder != null)
+        Keyboard keyboard = Keyboard.current;
+        if (keyboard != null)
         {
-            highlightBorder.gameObject.SetActive(true);
-            isHighlighted = true;
+            return keyboard.leftCtrlKey.isPressed || keyboard.rightCtrlKey.isPressed;
         }
-    }
-
-    public void DisableHighlight()
-    {
-        if (highlightBorder != null)
-        {
-            highlightBorder.gameObject.SetActive(false);
-            isHighlighted = false;
-        }
-    }
-
-    // Метод для сброса вращения
-    public void ResetRotation()
-    {
-        transform.rotation = Quaternion.Euler(0f, 0f, originalZRotation);
-    }
-
-    // Метод для сброса порядка в слое
-    public void ResetSortingOrder()
-    {
-        cardSprite.sortingOrder = normalSortingOrder;
-        originalSortingOrder = normalSortingOrder;
+        return false;
     }
 }
